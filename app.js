ec84595dd1eab7147197b242cfa0e213a9ede45e
Wasi khan <wasikhan7099@gmail.com>
2024-05-09 14:35:00 -0700

    day 2

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 // CODEE DAY 1
 
-const longestWord = (str) => {
-if(str.trim().length == 0){
-    return false;
-}
-words = str.split(' ');
-return words.reduce((acum,curWord) => (curWord.length > acum.length ?curWord:acum),"")
+// const longestWord = (str) => {
+// if(str.trim().length == 0){
+    // return false;
+// }
+// words = str.split(' ');
+// return words.reduce((acum,curWord) => (curWord.length > acum.length ?curWord:acum),"")
 // words = words.sort((a,b) => b.length - a.length);
 
 // console.log(words)
 // return words[0]
+// }
+// console.log(longestWord("this is a xxxxxx Samm aderson"))
+
+
+
+
+// CODEE DAY 1
+
+function hashtagGenerate(str){
+if(str.length >280 || str.trim()=== 0 ){
+    return false;
 }
-console.log(longestWord("this is a xxxxxx Samm aderson"))
+str  = str.split(' ');
+str =  str.map((curElem) => 
+    curElem.replace(curElem[0], curElem[0].toUpperCase()));
+
+
+
+// ===================  second option
+
+
+
+// curElem.charAt(0).toUpperCase() + curElem.slice(1));
+
+
+str= ` #${str.join("")}`;
+console.log(str)
+}
+hashtagGenerate("muhammad wasi khan")
